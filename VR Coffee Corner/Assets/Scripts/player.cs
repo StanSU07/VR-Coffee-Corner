d24e7a67713b4457a68ee6ee784cd11e1fcb81c0
DikoBJ <65281066+DikoBJ@users.noreply.github.com>
2020-12-04 10:53:28 +0100

    Add files via upload

---
+++ VR Coffee Corner/Assets/Scripts/player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player: MonoBehaviour
+{
+    float moveHorizontal;
+    float moveVertical;
+    Vector3 direction;
+    public float movingSpeed;
+  
+   
+    
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+
+        //Moving code
+        moveHorizontal = Input.GetAxis("Horizontal");
+        moveVertical = Input.GetAxis("Vertical");
+        direction = new Vector3(moveHorizontal, 0, moveVertical);
+        gameObject.transform.Translate(direction.normalized * Time.deltaTime * movingSpeed);
+
+
+
+        //Raising Hand code 
+
+         if (Input.GetKey("space"))
+        {
+           print("Raise your hand!");
+        }  
+    }
+}
