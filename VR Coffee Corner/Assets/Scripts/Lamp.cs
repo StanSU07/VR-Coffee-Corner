e53570087c3cbdb58999c5c67d17d25de9439410
DikoBJ <65281066+DikoBJ@users.noreply.github.com>
2020-12-09 16:08:59 +0100

    Lamp

---
+++ VR Coffee Corner/Assets/Scripts/Lamp.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lamp : MonoBehaviour {
+
+    [HideInInspector]
+    public GameObject LampLight;
+
+    [HideInInspector]
+    public GameObject DomeOff;
+
+    [HideInInspector]
+    public GameObject DomeOn;
+
+    public bool TurnOn=false;
+    
+    
+
+	// Use this for initialization
+	public void TurnOnLamp () {
+        if(TurnOn ==false){
+            LampLight.SetActive(true);
+            DomeOff.SetActive(false);
+            DomeOn.SetActive(true);
+        }
+        else if(TurnOn==true){
+            LampLight.SetActive(false);
+            DomeOff.SetActive(true);
+            DomeOn.SetActive(false);
+
+        }
+    }
+	
+}
