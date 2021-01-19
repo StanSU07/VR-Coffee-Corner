bf69fb71d1033c8909b3981b6e705827c0a5c580
DikoBJ <Amralina.Diana@gmail.com>
2021-01-19 13:18:35 +0100

    Poster

---
+++ VR Coffee Corner/Assets/Scripts/Poster.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Poster : MonoBehaviour
+{
+public SpriteRenderer spriteRenderer;
+public Sprite[] spriteArray;
+public int currentSprite;
+
+
+public void ChangeSprite()
+{
+    spriteRenderer.sprite = spriteArray[currentSprite];
+    currentSprite++;
+
+    if(currentSprite >= spriteArray.Length)
+    {
+        currentSprite = 0;
+    }
+}
+}
